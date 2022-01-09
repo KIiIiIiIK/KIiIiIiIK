47b6dea6d2b768e85d0228565cc31c39a1cbc322
KIiIiIiIK <96996328+KIiIiIiIK@users.noreply.github.com>
2022-01-10 01:09:51 +0200

    Update Clcltr.java

--- Clcltr.java
+++ Clcltr.java
@@ -1,63 +1,69 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+    import java.util.InputMismatchException;
+    import java.util.Scanner;
 
-public class Clcltr {
-    private String operation;
-    private Scanner scanner;
+    public class Clcltr {
+        private String operation;
+        private Scanner scanner;
 
-    public Clcltr() {
-        this.scanner = new Scanner(System.in);
-    }
-
-    public void calculate() throws InputMismatchException {
-        while (!"q".equals(operation)) {
-            System.out.println(calculationStep());
-        }
-    }
-
-    private double calculationStep() throws InputMismatchException {
-        System.out.println("enter first number:");
-        int firstNumber = Integer.parseInt(scanner.nextLine());
-        System.out.println("enter second number:");
-        int secondNumber = Integer.parseInt(scanner.nextLine());
-        System.out.println("enter operation ('+' or '-' or '/' or '*') to perform or q if you want to quit:");
-
-        operation = scanner.nextLine();
-
-        return performOperation(firstNumber, secondNumber);
-    }
-
-    private double performOperation(int firstNumber, int secondNumber) {
-        double result;
-
-        switch (operation){
-            case "+":
-                result = plus(firstNumber, secondNumber);
-                break;
-            case "-":
-                result = minus(firstNumber, secondNumber);
-                break;
-            case "/":
-                result = divide(firstNumber, secondNumber);
-                break;
-            case "*":
-                result = multiply(firstNumber, secondNumber);
-                break;
-            default:
-                result = 0;
-                break;
+        public Clcltr() {
+            this.scanner = new Scanner(System.in);
         }
 
-        return result;
-    }
+        public void calculate() throws InputMismatchException {
+            while (!"q".equals(operation)) {
+                System.out.println(calculationStep());
+            }
+        }
 
-    private int plus(int firstNumber, int secondNumber){
-        return firstNumber + secondNumber;}
-    private int minus(int firstNumber, int secondNumber){
-        return firstNumber - secondNumber;}
-    private int divide(int firstNumber, int secondNumber){
-        return firstNumber / secondNumber;}
-    private int multiply(int firstNumber, int secondNumber){
-        return firstNumber * secondNumber;}
-}
+        private double calculationStep() throws InputMismatchException {
+            System.out.println("enter first number:");
+            double firstNumber = Double.parseDouble(scanner.nextLine());
+            System.out.println("enter second number:");
+            double secondNumber = Double.parseDouble(scanner.nextLine());
+            System.out.println("enter operation ('+' or '-' or '/' or '*' or '%') " +
+                    "to perform or q if you want to quit:");
+
+            operation = scanner.nextLine();
+
+            return performOperation(firstNumber, secondNumber);
+        }
+
+        private double performOperation(double firstNumber, double secondNumber) {
+            double result;
+
+            switch (operation){
+                case "+":
+                    result = plus(firstNumber, secondNumber);
+                    break;
+                case "-":
+                    result = minus(firstNumber, secondNumber);
+                    break;
+                case "/":
+                    result = divide(firstNumber, secondNumber);
+                    break;
+                case "*":
+                    result = multiply(firstNumber, secondNumber);
+                    break;
+                case "%":
+                    result = percentage(firstNumber, secondNumber);
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+
+            return result;
+        }
+
+        private double plus(double firstNumber, double secondNumber){
+            return firstNumber + secondNumber;}
+        private double minus(double firstNumber, double secondNumber){
+            return firstNumber - secondNumber;}
+        private double divide(double firstNumber, double secondNumber){
+            return firstNumber / secondNumber;}
+        private double multiply(double firstNumber, double secondNumber){
+            return firstNumber * secondNumber;}
+        private double percentage(double firstNumber, double secondNumber){
+            return (firstNumber * secondNumber/100);}
+    }
 
